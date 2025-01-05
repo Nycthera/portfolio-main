8e8af8165fa9899065510c4c59d28862e636b618
dabby12 <liuweimi.sg@gmail.com>
2025-01-05 21:25:10 +0800

    removed ian and added placeholder image

--- src/components/HeroSection/HeroImage.jsx
+++ src/components/HeroSection/HeroImage.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import Ian from '../../assets/Ian.jpg'; // Correct relative path
 
 const HeroImage = () => {
   return (
     <div className="hidden lg:block w-64 h-64 lg:w-96 lg:h-96 rounded-full overflow-hidden shadow-lg">
       <img
-        src={Ian} // Use the imported image here
+        src="https://placehold.co/600x400"
         alt="Your Name"
         className="w-full h-full object-cover"
       />
     </div>
   );
 };
+
 console.error("Image: Ian.png cannot be rendered in due to 4.7Gb of file size. Error code: 402");
+
 export default HeroImage;
